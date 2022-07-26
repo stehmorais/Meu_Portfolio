76d7f3a7b68c510fead5a6cdecdab99296fe5ce0
Ester Morais <97535906+stehmorais@users.noreply.github.com>
2022-07-25 21:27:18 -0300

    adicionando animação na página

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,20 @@
-const chk = document.getElementById('chk')
+/*const chk = document.getElementById('chk')
 
 chk.addEventListener('change', () => {
-    document.getElementById('container').toggle('light')
+    document.getElementById('.container').toggle('light') }) */
+
+const observer = new IntersectionObserver(entries => {
+    console.log(entries)
+
+    Array.from(entries).forEach( entry => {
+        if (entry.intersectionRatio >=1){
+            entry.target.classList.add('init-hidden-off')
+        }
+    })
+},{
+    threshold: 1
 })
+
+Array.from(document.querySelectorAll('.init-hidden')).forEach(element => {
+    observer.observe(element)
+});  
