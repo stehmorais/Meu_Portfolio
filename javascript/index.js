50253b64554abbfc5976d6a388ba5e82cda5de7c
Ester Morais <97535906+stehmorais@users.noreply.github.com>
2022-07-28 22:31:19 -0300

    adicionando funcionalidades de troca de tema

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,30 @@
-const moon = document.getElementById('')
-const container = document.getElementById('container')
-
-function light () {
-    container.style.backgroundColor = 'white';
-}
-
-
-chk.addEventListener('click', light);
-
+const tema = document.getElementById('tema');
 const observer = new IntersectionObserver(entries => {
-   
 
-    Array.from(entries).forEach( entry => {
-        if (entry.intersectionRatio >=1){
+    Array.from(entries).forEach(entry => {
+        if (entry.intersectionRatio >= 1) {
             entry.target.classList.add('init-hidden-off')
         }
     })
-},{
+}, {
     threshold: 1
 })
 
 Array.from(document.querySelectorAll('.init-hidden')).forEach(element => {
     observer.observe(element)
-});  
+});
 
+function lightDark (i){
+    if ( i == 1){
+        container.style.backgroundColor = "white";
+        container.style.color = 'black'; 
+    } else if (i == 0) {
+        document.getElementById("tema").src="./images/sun.svg";
+        container.style.backgroundColor = "black";
+        container.style.color = 'white';  
+    }
+}
 
-    
-    
+// tema.addEventListener('click', light);
+lightDark.addEventListener('click', lightDark);
+
